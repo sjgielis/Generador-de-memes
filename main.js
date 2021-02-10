eddcecf48ed77f82615fbcc50720a6c59e09e382
Sa <sjgielis@gmail.com>
2021-02-10 01:36:53 -0300

    avance en JS

--- main.js
+++ main.js
@@ -2,15 +2,116 @@ const textoSuperior = document.getElementById("textoSuperior");
 const ingresoTitulo = document.getElementById("ingresoTitulo");
 const textoInferior = document.getElementById("textoInferior");
 const texInferior = document.getElementById("texInferior");
+const topCheck = document.getElementById("top-check");
+const topCheckInf = document.getElementById("top-check-2");
+const estiloFuente = document.getElementById("estilosDeFuentes");
+const tamañoLetra = document.getElementById("tamañoLetra");
+const izquierda = document.getElementById("izquierda");
+const derecha = document.getElementById("derecha");
+const centrar = document.getElementById("centrar");
+const colorLetra = document.getElementById("colorTexto");
+const textoColor = document.getElementById("textoColor");
+const colorFondo = document.getElementById("colorFondo");
+const colorLabelFondo = document.getElementById("colorLaberFondo");
 
+
+
+
+// Modificaciones Texto
 
 ingresoTitulo.addEventListener(`keyup`,()=>{
     textoSuperior.innerHTML=ingresoTitulo.value;
-})
+});
 
 texInferior.addEventListener(`keyup`,()=>{
     textoInferior.innerHTML=texInferior.value;
-})
+});
 
 
 
+topCheck.addEventListener('change', () => {
+    if (topCheck.checked === true) {
+        textoSuperior.style.display = 'none';
+    } else {
+        textoSuperior.style.display = 'block';
+    }
+});
+
+topCheckInf.addEventListener('change', () => {
+    if (topCheckInf.checked === true) {
+        textoInferior.style.display = 'none';
+    } else {
+        textoInferior.style.display = 'block';
+    }
+});
+
+
+
+estiloFuente.addEventListener('change', (e) => {
+    const fuente = e.target.value;
+    textoSuperior.style.fontFamily = `${fuente}`;
+    textoInferior.style.fontFamily = `${fuente}`;
+    // if (fuente === 'arial') {
+    //     textoSuperior.style.fontFamily = 'arial';
+    //     textoInferior.style.fontFamily = 'arial';
+    // } else if (fuente === 'arial-black') {
+    //     textoSuperior.style.fontFamily = 'Arial Black';
+    //     textoInferior.style.fontFamily = 'Arial Black';
+    // } else if (fuente === 'american-type') {
+    //     textoSuperior.style.fontFamily = `American Typewriter`;
+    //     textoInferior.style.fontFamily = `American Typewriter`;
+    // } else if (fuente === 'andale-mono') {
+    //     textoSuperior.style.estiloFuente = `Andale Mono`;
+    //     textoInferior.style.estiloFuente = `Andale Mono`;
+    // } else if (fuente === 'helvetica') {
+    //     textoSuperior.style.estiloFuente = `Helvetica`;
+    //     textoInferior.style.estiloFuente = `Helvetica`;
+    // } else if (fuente === 'impact') {
+    //     textoSuperior.style.estiloFuente = `Impact`;
+    //     textoInferior.style.estiloFuente = `Impact`;
+    // } 
+});
+
+
+tamañoLetra.addEventListener('change', (e) => {
+    const size = e.target.value;
+    textoSuperior.style.fontSize = `${size}px`;
+    textoInferior.style.fontSize = `${size}px`;
+});
+
+
+izquierda.addEventListener('click', (e) => {
+    e.preventDefault();
+    textoSuperior.style.textAlign = 'left';
+    textoInferior.style.textAlign = 'left';
+  });
+  
+derecha.addEventListener('click', (e) => {
+    e.preventDefault();
+    textoSuperior.style.textAlign = 'right';
+    textoInferior.style.textAlign = 'right';
+  });
+  
+centrar.addEventListener('click', (e) => {
+    e.preventDefault();
+    textoSuperior.style.textAlign = 'center';
+    textoInferior.style.textAlign = 'center';
+  });
+
+
+
+colorLetra.addEventListener('input', (e) =>{
+    const colorTexto = e.target.value;
+    textoSuperior.style.color = colorTexto;
+    textoInferior.style.color = colorTexto;
+    textoColor.innerHTML = colorTexto.toUpperCase();
+  })
+  
+  
+  
+  fondoLabel.addEventListener('input', (e) =>{
+    const colorFondo = e.target.value;
+    textoSuperior.style.backgroundColor = colorFondo;
+    textoInferior.style.backgroundColor = colorFondo;
+    colorLabelFondo.innerHTML = colorFondo.toUpperCase();
+  })
